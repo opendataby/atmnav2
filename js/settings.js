96a65bbc72bf69708ba60d334ea1c3d5198979c2
Max Usachev <maxusachev@gmail.com>
2012-11-05 19:03:18 +0300

    Increased request timeout

--- js/settings.js
+++ js/settings.js
@@ -6,7 +6,7 @@ app.settings = {
     version: '2.0',
 
     vibrateMilliseconds: 40,
-    ajaxTimeout: 7000, // 7 sec
+    ajaxTimeout: 10000, // 10 sec
 
     mapTileUrlTemplate: 'http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png',
 
