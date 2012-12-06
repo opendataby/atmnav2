f9166f647bad0eac70c11d7ced58fd73047a1212
Max Usachev <maxusachev@gmail.com>
2012-12-06 23:21:41 +0300

    Added error tracking using Google Analytics

--- js/app.js
+++ js/app.js
@@ -3,3 +3,23 @@ app.utils.setDefaults();
 jQuery.ajaxSetup({
     timeout: app.settings.ajaxTimeout
 });
+
+window.onerror = function(errorMsg, url, lineNumber) {
+    jQuery.ajax({
+        'url': app.settings.errorsUrl,
+        'type': 'POST',
+        'async': true,
+        'dataType': 'json',
+        'timeout': app.settings.ajaxTimeout,
+        'data': {
+            'error_msg': errorMsg,
+            'error_url': url,
+            'error_line': lineNumber
+        },
+        'error': function(jqXHR, textStatus, errorThrown) {
+            alert(textStatus);
+        }
+    });
+
+    return true;
+};
