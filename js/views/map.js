cc0a71b3efd25417c446dd66c6332e108e053305
Max Usachev <maxusachev@gmail.com>
2012-12-25 16:12:25 +0300

    Temporarily disable Create mode

--- js/views/map.js
+++ js/views/map.js
@@ -107,14 +107,14 @@
             var self = this;
             if (L.Browser.mobile) {
                 this.addMapControl(tr('Locate'), 'topright', 'locate-icon', self.moveToLocation, self);
-                this.addMapControl(tr('Request new point'), 'topleft', 'position-add', self.toCreateMode, self);
+                //this.addMapControl(tr('Request new point'), 'topleft', 'position-add', self.toCreateMode, self);
                 this.addMapControl(tr('Zoom Out'), 'bottomright', 'zoom-out-icon', this.map.zoomOut);
                 this.addMapControl(tr('Zoom In'), 'bottomright', 'zoom-in-icon', this.map.zoomIn);
             } else {
                 this.addMapControl(tr('Zoom In'), 'topleft', 'zoom-in-icon', this.map.zoomIn);
                 this.addMapControl(tr('Zoom Out'), 'topleft', 'zoom-out-icon', this.map.zoomOut);
                 this.addMapControl(tr('Locate'), 'topleft', 'locate-icon', self.moveToLocation, self);
-                this.addMapControl(tr('Request new point'), 'topright', 'position-add', self.toCreateMode, self);
+                //this.addMapControl(tr('Request new point'), 'topright', 'position-add', self.toCreateMode, self);
             }
 
             app.utils.log('map:addMapControls:end');
