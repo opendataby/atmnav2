6dab0dd8b20a177458bc99a144c756ad1073beb7
Max Usachev <maxusachev@gmail.com>
2013-01-31 17:25:56 +0300

    #81: Disable submit button after form submit

--- js/views/create.js
+++ js/views/create.js
@@ -12,13 +12,34 @@
             return _.template($('#create-template').html())();
         },
 
+        attach: function(container) {
+            app.utils.log('create:attach:start');
+
+            app.PageView.prototype.attach.call(this, container);
+            this.toggleSubmitButton(true);
+
+            app.utils.log('create:attach:end');
+            return this;
+        },
+
+        toggleSubmitButton: function(enable) {
+            var button = $('button', this.el);
+
+            if (enable) {
+                button.removeAttr('disabled');
+            } else {
+                button.attr('disabled', 'disabled');
+            }
+        },
+
         onSubmit: function(event) {
             console.log('create:onSubmit');
 
             event.preventDefault();
             event.stopPropagation();
 
-            var form = this.$el.find('.nt-create-form'),
+            var self = this,
+                form = this.$el.find('.nt-create-form'),
                 type = $('input[name=type]', form).val(),
                 provider = $('input[name=provider]', form).val(),
                 address = $('input[name=address]', form).val(),
@@ -35,6 +56,7 @@
                 var onSuccess = function(data) {
                     if (data.success) {
                         form.find('input').val('');
+                        self.toggleSubmitButton(false);
                         app.utils.alert(tr('Thank you for submit'), tr('Message'));
                     } else {
                         onError();
